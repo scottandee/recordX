1e425230cf741f9cfc9454501a6c98d47b36d37c
Olayinkascott Andee <andeeolayinkascott@gmail.com>
2023-10-30 23:55:22 +0100

    Add CORS configuration to app

--- backend/run_app.py
+++ backend/run_app.py
@@ -4,10 +4,12 @@ app and runs it"""
 
 from app import create_app, db
 from config import Config
+from flask_cors import CORS
 
 
 if __name__ == "__main__":
     app = create_app(Config)
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
     with app.app_context():
         db.create_all()
     app.run(host="0.0.0.0", port=5000, debug=True)
